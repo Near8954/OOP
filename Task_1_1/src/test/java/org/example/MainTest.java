ef5ecbe4b4c739dd235c41c8f71c01b85ff5a0d5
Roman Baldin <roma.baldin@gmail.com>
2026-09-10 20:08:26 +0700

    fixed testing

--- Task_1_1/src/test/java/org/example/MainTest.java
+++ Task_1_1/src/test/java/org/example/MainTest.java
@@ -11,22 +11,75 @@ import java.util.Arrays;
 
 class MainTest {
 
-    @Test
-    void heapSort() {
-        int maxArraySize = 1001;
-        for (int arraySize = 0; arraySize < maxArraySize; ++arraySize) {
-            int testsCount = 100;
-            int[] given = new int[arraySize];
-            Random randomise = new Random(6769);
-            for (int i = 0; i < testsCount; ++i) {
-                for (int j = 0; j < arraySize; ++j) {
-                    given[j] = randomise.nextInt();
-                }
-                int[] expected = given.clone();
-                Arrays.sort(expected);
-                HeapSort.heapSort(given);
-                assertArrayEquals(given, expected);
-            }
+    private int[] genRandomArray(int size, long seed) {
+        Random randomise = new Random(seed);
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = randomise.nextInt();
         }
+        return arr;
+    }
+
+    @Test
+    void testEmptyArray() {
+        int[] given = {};
+        int[] expected = {};
+        HeapSort.heapSort(given);
+        assertArrayEquals(expected, given);
+    }
+
+    @Test
+    void testOneElementArray() {
+        int[] given = {42};
+        int[] expected = {42};
+        HeapSort.heapSort(given);
+        assertArrayEquals(expected, given);
+    }
+
+    @Test
+    void testTwoElementsArrayAlreadySorted() {
+        int[] given = {1, 2};
+        int[] expected = {1, 2};
+        HeapSort.heapSort(given);
+        assertArrayEquals(expected, given);
+    }
+
+    @Test
+    void testTwoElementsArrayUnsorted() {
+        int[] given = {2, 1};
+        int[] expected = {1, 2};
+        HeapSort.heapSort(given);
+        assertArrayEquals(expected, given);
+    }
+
+    @Test
+    void testRandomArraySmall() {
+        int[] given = genRandomArray(10, 12345L);
+        int[] expected = given.clone();
+        Arrays.sort(expected);
+
+        HeapSort.heapSort(given);
+        assertArrayEquals(expected, given);
+    }
+
+    @Test
+    void testRandomArrayMedium() {
+        int[] given = genRandomArray(100, 6769L);
+        int[] expected = given.clone();
+        Arrays.sort(expected);
+
+        HeapSort.heapSort(given);
+        assertArrayEquals(expected, given);
+    }
+
+    @Test
+    void testRandomArrayLarge() {
+        int[] given = genRandomArray(1000, 98765L);
+        int[] expected = given.clone();
+        Arrays.sort(expected);
+
+        HeapSort.heapSort(given);
+        assertArrayEquals(expected, given);
     }
 }
+
