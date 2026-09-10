ab6c87c61d2846a71a741b909e1983fceae826d0
Near8954 <Near8954@users.noreply.github.com>
2026-09-10 12:50:56 +0000

    Deploying to gh-pages from @ Near8954/OOP@069b1d04c624dfd26cb1fa3f927931f4f8d367b8 🚀

--- Task_1_1/type-search-index.js
+++ Task_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"heapSort"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Utils"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"HeapSort"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Utils"}];updateSearchResults();
