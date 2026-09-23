db4b079500768ac8fff04e7cdd86d47e735b344b
Roman Baldin <roma.baldin@gmail.com>
2026-09-23 19:19:02 +0700

    fixes

--- Task_1_2/src/test/java/org/example/DeckTest.java
+++ Task_1_2/src/test/java/org/example/DeckTest.java
@@ -1,4 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import java.util.EmptyStackException;
 import static org.junit.jupiter.api.Assertions.*;
+
 class DeckTest {
-  
+
+    @Test
+    void testDeckInitialization() {
+        Deck deck = new Deck();
+        for (int i = 0; i < 52; i++) {
+            assertNotNull(deck.getCard());
+        }
+        assertThrows(EmptyStackException.class, deck::getCard);
+    }
+
+    @Test
+    void testShuffleDeck() {
+        Deck deck = new Deck();
+        assertDoesNotThrow(deck::shuffleDeck);
+        assertNotNull(deck.getCard());
+    }
 }
--- Task_1_2/src/test/java/org/example/GameTest.java
+++ Task_1_2/src/test/java/org/example/GameTest.java
@@ -1,4 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import static org.junit.jupiter.api.Assertions.*;
+
 class GameTest {
-  
+
+    private final InputStream originalSystemIn = System.in;
+
+    @AfterEach
+    void tearDown() {
+        System.setIn(originalSystemIn);
+    }
+
+    @Test
+    void testGameExecutionAndExit() {
+        String simulatedInput = "0\n0\n0\n";
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
+
+        Game game = new Game();
+        assertDoesNotThrow(game::startGame);
+    }
 }
